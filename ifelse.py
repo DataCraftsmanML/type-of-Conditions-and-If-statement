20176a88a261e1adc7e38505782fd5be15964173
Jiya Savaliya <126229948+Jiyasavaliya@users.noreply.github.com>
2024-06-04 00:28:45 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,35 @@
+"""if 10<3: print("true")
+elif 10<12: print("ok")
+else: print("no")
+
+"""
+
+"""print("yes") if 10<3 else print("no")"""
+
+"""print("A") if 10 > 30 else print("=") if 10 == 10 else print("B")"""
+
+"""print("both condition are true") if 10 < 30 and 10 > 30  else print("both condition are true")"""
+
+"""print("both condition are true") if 10 < 3 or 10 > 30  else print("both condition are flase")"""
+
+"""print("this condition is true") if not 10 > 3 else print("this condition is flase")"""
+
+"""if 10 > 2: print("10 gretherthen 2")
+if 10 > 3: print("10 gretherthen 3")
+else: print("Both condition are flase")"""
+
+"""a = 200
+b = 33
+c = 500
+if a > b or a > c: print("At least one of the conditions is True")
+"""
+
+"""x = 41
+if x > 10: print("Above ten,")
+if x > 20: print("and also above 20!")
+else: print("but not above 20.")
+"""
+
+"""if 10 > 3:
+    pass"""
+
